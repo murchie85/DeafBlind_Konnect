219a285fcbbdd2d7b51c1bc560753cbb8531322b
Adam <murchie85@gmail.com>
2018-06-30 18:33:09 +0100

    first commit

---
+++ app/src/main/java/com/example/android/db_konnect/Message.java
@@ -0,0 +1,26 @@
+package com.example.android.db_konnect;
+
+
+public class Message {
+    private String text;
+    private MemberData data;
+    private boolean belongsToCurrentUser;
+
+    public Message(String text, MemberData data, boolean belongsToCurrentUser) {
+        this.text = text;
+        this.data = data;
+        this.belongsToCurrentUser = belongsToCurrentUser;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public MemberData getData() {
+        return data;
+    }
+
+    public boolean isBelongsToCurrentUser() {
+        return belongsToCurrentUser;
+    }
+}
